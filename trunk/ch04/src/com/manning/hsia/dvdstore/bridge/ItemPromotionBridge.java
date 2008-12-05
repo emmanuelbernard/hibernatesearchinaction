3a58f6e21e7023c3570c8c5ad3fc4cfd3271ea0c
sanne.grinovero@gmail.com <sanne.grinovero@gmail.com@7b7ec324-ace7-11dd-ac80-e787e3ad770f>
2008-12-05 16:43:10 +0000

    unused imports and code cleanup, chapter 04

--- trunk/ch04/src/com/manning/hsia/dvdstore/bridge/ItemPromotionBridge.java
+++ trunk/ch04/src/com/manning/hsia/dvdstore/bridge/ItemPromotionBridge.java
@@ -1,9 +1,6 @@
 package com.manning.hsia.dvdstore.bridge;
 
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Index;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.Field.TermVector;
 import org.hibernate.search.bridge.FieldBridge;
 import org.hibernate.search.bridge.LuceneOptions;
 
